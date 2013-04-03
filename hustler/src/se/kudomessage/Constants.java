7d24dcdaa3422977abd56af97ba11e7d8701b872
Marcus Olsson <marcus@meeloo.se>
2013-04-03 19:33:50 +0200

    Updated the file containing constants.
    Prepared it for usage with sockets.

--- hustler/src/se/kudomessage/Constants.java
+++ hustler/src/se/kudomessage/Constants.java
@@ -2,6 +2,9 @@ package se.kudomessage;
 
 public class Constants {
 	public static String HOST = "imap.gmail.com";
-	public static String USERNAME = "kudomessage@gmail.com";
-	public static String OAUTHTOKEN = "";
+	public static int SOCKETPORT = 5000;
+	public static String DBHOST = "";
+	public static String DBDATABASE = "";
+	public static String DBUSERNAME = "";
+	public static String DBPASSWORD = "";
 }
