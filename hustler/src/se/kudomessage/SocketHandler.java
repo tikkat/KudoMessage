a53985ec26d2240665027248a9781c98d41fd12c
Marcus Olsson <marcus@meeloo.se>
2013-04-03 19:43:41 +0200

    Added socket functionality.
    This it the way for clients and the server application (gateway) to
    communicate with Hustler.
    
    Connect, send a appropriate command and then a JSON object as a string.

---
+++ hustler/src/se/kudomessage/SocketHandler.java
@@ -0,0 +1,76 @@
+package se.kudomessage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketHandler {
+	public SocketHandler() throws IOException {
+		ServerSocket serverSocket = null;
+		boolean listening = true;
+
+		try {
+			serverSocket = new ServerSocket(Constants.SOCKETPORT);
+		} catch (IOException e) {
+			System.err.println("Could not listen on port: " + Constants.SOCKETPORT + ".");
+			System.exit(-1);
+		}
+
+		while (listening)
+			new ClientHandler(serverSocket.accept()).start();
+
+		serverSocket.close();
+	}
+
+	private class ClientHandler extends Thread {
+		private Socket socket;
+		private PrintWriter out;
+		private BufferedReader in;
+		
+		private ClientUser cu;
+
+		public ClientHandler(Socket socket) {
+			this.socket = socket;
+		}
+
+		public void run() {
+			String inputLine;
+			
+			try {
+				out = new PrintWriter(socket.getOutputStream(), true);
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+				while (true) {
+					inputLine = in.readLine();
+					
+					switch (inputLine) {
+						case "INITIALIZE":			cu = new ClientUser(in.readLine(), out);
+						break;
+						case "REGISTER_SERVER":		cu.registerServer(in.readLine(), out);
+						break;
+						case "SEND_MESSAGE":		cu.sendMessage(in.readLine(), out);
+						break;
+						case "SENT_MESSAGE":		cu.sentMessage(in.readLine(), out);
+						break;
+						case "RECEIVED_MESSAGE":	cu.receivedMessage(in.readLine(), out);
+						break;
+					}
+					
+					if (inputLine.equals("CLOSE")) {
+						out.write("OK");
+						break;
+					}
+				}
+				
+				out.close();
+				in.close();
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
