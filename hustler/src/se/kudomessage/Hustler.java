c3c4dcbdd289ac4cd1cec6d509929cfb3e2fb437
Marcus Olsson <marcus@meeloo.se>
2013-04-03 19:39:45 +0200

    Removed testing lines.

--- hustler/src/se/kudomessage/Hustler.java
+++ hustler/src/se/kudomessage/Hustler.java
@@ -6,23 +6,8 @@ import javax.mail.MessagingException;
 
 public class Hustler {
 
-	/**
-	 * @param args
-	 * @throws MessagingException 
-	 * @throws IOException 
-	 * @throws InterruptedException 
-	 */
-	public static void main(String[] args) throws MessagingException, IOException, InterruptedException {
-		// TODO Auto-generated method stub
-		
-		GmailHandler gh = new GmailHandler();
-		
-		// Ladda upp ett sms till pending på gmail och behåll id't
-		int id = gh.saveMessageToPending("Mejl att flytta!", "receiver", "sender");
-		
-		// Flytta smset med id't till standard
-		gh.moveMessage(id, GmailHandler.Labels.PENDING, GmailHandler.Labels.STANDARD);
-		System.out.println("Flyttade mejlet..");
+	public static void main(String[] args) throws IOException, MessagingException {
+		new SocketHandler();
 	}
 
 }
