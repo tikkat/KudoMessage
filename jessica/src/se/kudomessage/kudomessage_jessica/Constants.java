ab887127b82259978611429da6b0f7d78f24f7fa
Marcus Olsson <marcus@meeloo.se>
2013-04-06 22:42:26 +0200

    Added sockets and registration with Hustler.

--- jessica/src/se/kudomessage/kudomessage_jessica/Constants.java
+++ jessica/src/se/kudomessage/kudomessage_jessica/Constants.java
@@ -3,4 +3,5 @@ package se.kudomessage.kudomessage_jessica;
 public class Constants {
 	public static final String SENDER_ID = "";
 	public static final String IP_ADDRESS = "";
+	public static final int PORT = "";
 }
--- jessica/src/se/kudomessage/kudomessage_jessica/MainActivity.java
+++ jessica/src/se/kudomessage/kudomessage_jessica/MainActivity.java
@@ -1,5 +1,12 @@
 package se.kudomessage.kudomessage_jessica;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import com.google.android.gcm.GCMRegistrar;
 
 import android.os.Bundle;
@@ -7,6 +14,10 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	Socket socket = null;
+    PrintWriter out = null;
+    BufferedReader in = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +30,25 @@ public class MainActivity extends Activity {
 		String regId = GCMRegistrar.getRegistrationId(this);
 		
 		if (regId.equals("")) {
-			GCMRegistrar.register(this, Constants.IP_ADDRESS);
+			GCMRegistrar.register(this, Constants.SENDER_ID);
 		}
+		
+		try {
+            socket = new Socket(Constants.IP_ADDRESS, Constants.PORT);
+            
+            out = new PrintWriter(socket.getOutputStream(), true);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch(UnknownHostException e) {
+            System.err.println("Couldn't find the host.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		
+		out.println("INITIALIZE");
+		out.println("API-KEY");
+		
+		out.println("REGISTER_ANDROID_DEVICE");
+		out.println(regId);
 	}
 
 	@Override
