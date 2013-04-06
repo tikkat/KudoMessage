8d394963c47bdbdde1e631cc732d09425d5e1444
Alexnader Hultnér <ahultner@gmail.com>
2013-04-06 23:05:42 +0200

    Updated port in constants

--- jessica/src/se/kudomessage/kudomessage_jessica/Constants.java
+++ jessica/src/se/kudomessage/kudomessage_jessica/Constants.java
@@ -3,5 +3,5 @@ package se.kudomessage.kudomessage_jessica;
 public class Constants {
 	public static final String SENDER_ID = "";
 	public static final String IP_ADDRESS = "";
-	public static final int PORT = "";
+	public static final int PORT = 0;
 }
