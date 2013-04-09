1c8488ba409e03beea8eb96954523b1d703163a7
Marcus Olsson <marcus@meeloo.se>
2013-04-09 16:06:25 +0200

    Updated the socket streams to be in UTF-8.

--- hustler/src/se/kudomessage/SocketHandler.java
+++ hustler/src/se/kudomessage/SocketHandler.java
@@ -3,6 +3,7 @@ package se.kudomessage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -39,8 +40,9 @@ public class SocketHandler {
 
 		public void run() {
 			try {
-				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				out = new PrintWriter(socket.getOutputStream(), true);
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+				OutputStreamWriter outstream = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+				out = new PrintWriter(outstream, true);
 
 				cu = new ClientUser(in, out, socket);
 			} catch (IOException e) {
