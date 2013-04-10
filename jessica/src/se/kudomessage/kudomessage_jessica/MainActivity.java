e4ed54dcc35f0f88239302d535191bda3b2ecd11
Marcus Olsson <marcus@meeloo.se>
2013-04-10 16:42:52 +0200

    Made some changes for cleaner code.

--- jessica/src/se/kudomessage/kudomessage_jessica/MainActivity.java
+++ jessica/src/se/kudomessage/kudomessage_jessica/MainActivity.java
@@ -1,12 +1,5 @@
 package se.kudomessage.kudomessage_jessica;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ConnectException;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
 import com.google.android.gcm.GCMRegistrar;
 
 import android.os.Bundle;
@@ -18,9 +11,7 @@ import android.app.Activity;
 import android.util.Log;
 
 public class MainActivity extends Activity {
-	Socket socket = null;
-	PrintWriter out = null;
-	BufferedReader in = null;
+	SMSHandler smsHandler = null;
 
 	private String token;
 	private String GCMKey;
@@ -31,11 +22,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		new SMSSent(this);
 
 		GCMRegistrar.checkDevice(this);
 		GCMRegistrar.checkManifest(this);
+		
+		smsHandler = new SMSHandler(this);
 
 		this.initOAuth();
 
@@ -49,25 +40,8 @@ public class MainActivity extends Activity {
 			Log.v("SMSTagTracker", "Got an GCM-key: " + GCMKey);
 		else
 			Log.v("SMSTagTracker", "Didn't get an GCM-key");
-
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					socket = new Socket(Constants.IP_ADDRESS, Constants.PORT);
-					
-					out = new PrintWriter(socket.getOutputStream(), true);
-		            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		            
-		            Log.v("SMSTagTracker", "Connected to socket " + Constants.IP_ADDRESS + ":" + Constants.PORT);
-		        } catch (UnknownHostException e) {
-		        	Log.v("SMSTagTracker", "Couldn't find the host.");
-		        } catch (ConnectException e) {
-		        	Log.v("SMSTagTracker", "Failed to connect to socket " + Constants.IP_ADDRESS + ":" + Constants.PORT);
-				} catch (Exception e) {
-					Log.v("SMSTagTracker", "Exception", e);
-				}
-			}
-		}).start();
+		
+		new SocketHandler();
 	}
 
 	/**
