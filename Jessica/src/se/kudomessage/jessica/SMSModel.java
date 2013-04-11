9a63c3ab24ee04955241d3016eece03d48f4d8de
Alexnader Hultnér <ahultner@gmail.com>
2013-04-11 21:32:22 +0200

    SMSModel

---
+++ Jessica/src/se/kudomessage/jessica/SMSModel.java
@@ -0,0 +1,11 @@
+package se.kudomessage.jessica;
+
+public class SMSModel implements ISMSModel {
+
+	@Override
+	public void sendSMS(KudoMessage m) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
