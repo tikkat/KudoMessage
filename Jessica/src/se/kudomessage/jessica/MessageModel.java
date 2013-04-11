711bd1571b87ae8258a80819a75b039f840bdd78
Alexnader Hultnér <ahultner@gmail.com>
2013-04-11 21:29:28 +0200

    Added skeleton for MessageModel

---
+++ Jessica/src/se/kudomessage/jessica/MessageModel.java
@@ -0,0 +1,23 @@
+package se.kudomessage.jessica;
+
+public class MessageModel implements IMessageModel{
+
+	@Override
+	public void sendMessage(KudoMessage m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reveivedMessage(KudoMessage m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addMessage(KudoMessage m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
