23d0adc2da89f271a6522f073fddf6b494186b02
Alexnader Hultnér <ahultner@gmail.com>
2013-04-11 21:30:20 +0200

    Added skeleton for OAuthModel

---
+++ Jessica/src/se/kudomessage/jessica/OAuthModel.java
@@ -0,0 +1,23 @@
+package se.kudomessage.jessica;
+
+public class OAuthModel implements IOAuthModel{
+
+	@Override
+	public void requestAccessToken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void revokeAccessToken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void renewAccessToken() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
