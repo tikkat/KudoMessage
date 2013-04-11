a3f4fa01d15b96530a94d52c780b2ed85a41d91e
Alexnader Hultnér <ahultner@gmail.com>
2013-04-11 21:27:29 +0200

    Added skeleton for GmailModel

---
+++ Jessica/src/se/kudomessage/jessica/GmailModel.java
@@ -0,0 +1,35 @@
+package se.kudomessage.jessica;
+
+public class GmailModel implements IGmailModel {
+
+	@Override
+	public void addReceivedMessage(KudoMessage m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addSentMessage(KudoMessage m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void getMessage(KudoMessage m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void findMessage(String query) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void moveMessage(KudoMessage m, Label target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
