4ae348b26672262343ce5a33914bb1382c66b081
Alexnader Hultnér <ahultner@gmail.com>
2013-04-11 21:32:14 +0200

    Added skeleton for PushModel

---
+++ Jessica/src/se/kudomessage/jessica/PushModel.java
@@ -0,0 +1,17 @@
+package se.kudomessage.jessica;
+
+public class PushModel implements IPushModel {
+
+	@Override
+	public void pushMessage(KudoMessage m) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void registerServer() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
