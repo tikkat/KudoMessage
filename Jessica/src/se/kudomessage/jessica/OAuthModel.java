00fac66582ac90d9495c58f81a3e9e7b5c123f68
Marcus Olsson <marcus@meeloo.se>
2013-04-13 10:08:39 +0200

    Can now get and revoke an access token.

--- Jessica/src/se/kudomessage/jessica/OAuthModel.java
+++ Jessica/src/se/kudomessage/jessica/OAuthModel.java
@@ -1,23 +1,43 @@
 package se.kudomessage.jessica;
 
-public class OAuthModel implements IOAuthModel{
+import android.accounts.AccountManager;
+import android.accounts.AccountManagerCallback;
+import android.accounts.AccountManagerFuture;
+import android.accounts.OperationCanceledException;
+import android.os.Bundle;
 
+public class OAuthModel implements IOAuthModel{
+	private String oauthScope = "oauth2:https://mail.google.com https://www.google.com/m8/feeds https://www.googleapis.com/auth/userinfo.email";
+	
 	@Override
 	public void requestAccessToken() {
-		// TODO Auto-generated method stub
-		
+		AccountManager am = AccountManager.get(Globals.getActivity());
+		am.getAuthTokenByFeatures("com.google", oauthScope, null, Globals.getActivity(), null, null, new OAuthCallback(), null);
 	}
 
 	@Override
 	public void revokeAccessToken() {
-		// TODO Auto-generated method stub
-		
+		AccountManager am = AccountManager.get(Globals.getActivity());
+		am.invalidateAuthToken("com.google", Globals.getAccessToken());
 	}
 
 	@Override
 	public void renewAccessToken() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private class OAuthCallback implements AccountManagerCallback<Bundle> {
+		@Override
+		public void run(AccountManagerFuture<Bundle> result) {
+			try {
+				Globals.setAccessToken(result.getResult().getString(AccountManager.KEY_AUTHTOKEN));
+			} catch (OperationCanceledException e) {
+				// Was denied the access, didn't get an access token.
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
