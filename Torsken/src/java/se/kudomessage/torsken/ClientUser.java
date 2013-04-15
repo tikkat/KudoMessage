20d17fa88da988541ea066ef3d2a33ee4a37afef
Philip Ekman <ekman1991@gmail.com>
2013-04-15 17:03:46 +0200

    Created the client user class.

---
+++ Torsken/src/java/se/kudomessage/torsken/ClientUser.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kudomessage.torsken;
+
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Philip
+ */
+@SessionScoped
+public class ClientUser {
+    
+    
+    
+    public ClientUser () {
+        
+    }
+    
+}
