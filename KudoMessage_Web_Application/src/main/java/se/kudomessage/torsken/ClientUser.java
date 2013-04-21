b277b03382f4028814637763ced6bf263067a84d
Philip Ekman <ekman1991@gmail.com>
2013-04-21 20:54:33 +0200

    Every user will now get a unique Pushgroup

--- KudoMessage_Web_Application/src/main/java/se/kudomessage/torsken/ClientUser.java
+++ KudoMessage_Web_Application/src/main/java/se/kudomessage/torsken/ClientUser.java
@@ -1,11 +1,19 @@
 package se.kudomessage.torsken;
 
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+@SessionScoped
 public class ClientUser {
     private static ClientUser instance = null;
     private String accessToken;
+    private final String ID;
    
     protected ClientUser () {
-        
+        FacesContext fcontext = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) fcontext.getExternalContext().getSession(false);
+        ID = session.getId();
     }
     
     public static ClientUser getInstance () {
@@ -13,5 +21,9 @@ public class ClientUser {
             instance = new ClientUser();
         }
         return instance;
+    }
+    
+    public String getID () {
+        return ID;
     }
 }
