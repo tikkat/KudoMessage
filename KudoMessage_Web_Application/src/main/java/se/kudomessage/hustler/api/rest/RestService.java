1506ef59b6d7005161dbd02ce59d54cfd5f6d60b
Tobias Tikka <tobias.tikka@gmail.com>
2013-04-22 16:22:42 +0200

    Started implementing the real REST api

--- KudoMessage_Web_Application/src/main/java/se/kudomessage/hustler/api/rest/RestService.java
+++ KudoMessage_Web_Application/src/main/java/se/kudomessage/hustler/api/rest/RestService.java
@@ -1,33 +1,70 @@
 package se.kudomessage.hustler.api.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.json.JSONException;
+import org.json.JSONObject;
+import se.kudomessage.hustler.PushHandler;
 
 // Här skriver man in basurlen, nu är den på /api/rest/ enbart.
 @Path("/")
 public class RestService {
+    @POST
+    @Path("register-server")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String registerServer(String inputData) throws JSONException {
+        JSONObject response = new JSONObject();
+        JSONObject input = new JSONObject(inputData);
 
-    // Körs om TEXT_PLAIN efterfrågas
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String simplePlainText() {
-        return "Hello from KudoMessage API";
-    }
+        if (input.getString("type").equals("android")) {
+            PushHandler.registerAndroidServer(input.getString("token"), input.getString("gcm"));
+            response.put("response", "OK");
+        } else {
+            response.put("response", "ERROR");
+            response.put("message", "No valid type.");
+        }
 
-    // Körs om HTML efterfrågas
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public String simpleHtml() {
-        return "<html> " + "<title>" + "Hello from KudoMessage API" + "</title>"
-                + "<body><h1>" + "Hello from KudoMessage API" + "</body></h1>" + "</html> ";
+        return response.toString();
     }
     
-    // Körs om JSON efterfrågas
-    @GET
+    @POST
+    @Path("sent-message")
     @Produces(MediaType.APPLICATION_JSON)
-    public String simpleJSON() {
-        return "{ \"message\" : \"Hello from KudoMessage API\" }";
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String sentMessage(String inputData) throws JSONException {
+        JSONObject response = new JSONObject();
+        JSONObject input = new JSONObject(inputData);
+
+        response.put("response", "OK");
+        return response.toString();
+    }
+    
+    @GET
+    @Path("get-message")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String getMessage(String inputData) throws JSONException {
+        JSONObject response = new JSONObject();
+        JSONObject input = new JSONObject(inputData);
+
+        response.put("response", "OK");
+        return response.toString();
+    }
+    
+    @GET
+    @Path("get-messages")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String getMessages(String inputData) throws JSONException {
+        JSONObject response = new JSONObject();
+        JSONObject input = new JSONObject(inputData);
+
+        response.put("response", "OK");
+        return response.toString();
     }
 }
