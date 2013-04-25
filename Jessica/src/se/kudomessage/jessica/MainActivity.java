640ff734cfbf08062e40d81ffa41dfe633d76eac
Alexander Hultnér <ahultner@gmail.com>
2013-04-25 15:58:36 +0200

    Changed the way stuff are run so that we always wait for the callback before we try to use the data.

--- Jessica/src/se/kudomessage/jessica/MainActivity.java
+++ Jessica/src/se/kudomessage/jessica/MainActivity.java
@@ -2,6 +2,7 @@ package se.kudomessage.jessica;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import com.google.android.gcm.GCMRegistrar;
 
@@ -11,11 +12,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Globals.setActivity(this);
 
 		GCMRegistrar.checkDevice(this);
 		GCMRegistrar.checkManifest(this);
 		
-		OAuthModel.requestAccessToken();
+		GCMRegistrar.unregister(this);
+		
+		String GCMKey = GCMRegistrar.getRegistrationId(this);	
+		if(GCMKey.equals("")){
+			//Has an callback calling initOAuth
+			GCMRegistrar.register(this, CONSTANTS.SENDER_ID );
+		}else{
+			Globals.setGCM(GCMKey);
+			this.initOAuth();
+		}
+		
 	}
 
 	@Override
@@ -23,6 +35,14 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void init(){
+		PushModel.registerServer(); 
+	}
+
+	public void initOAuth() {
+		OAuthModel.requestAccessToken();
 	}
 
 }
