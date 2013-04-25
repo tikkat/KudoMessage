bb822b56f0f0708d0a15a37b2f2af2f3051516c6
Alexander Hultnér <ahultner@gmail.com>
2013-04-25 15:56:44 +0200

    Added support for apiPath and GCM as Global variables.
    Reason for apiPath not being an constant is because user will most
    likely eventually be able to use their own apiPath

--- Jessica/src/se/kudomessage/jessica/Globals.java
+++ Jessica/src/se/kudomessage/jessica/Globals.java
@@ -7,6 +7,8 @@ public class Globals{
 	private static String _accessToken;
 	private static String _email;
 	private static Activity _activity;
+	private static String _apiPath = "http://domain.com:8080/KudoMessage_Web_Application/api/rest/";
+	private static String GCM;
 
 	public static String getAccessToken() {
 		return _accessToken;
@@ -31,5 +33,20 @@ public class Globals{
 	public static void setActivity(Activity activity) {
 		_activity = activity;
 	}
+	
+	public static void setApiPath(String apiPath){
+		_apiPath = apiPath;
+	}
 
+	public static String getApiPath(){
+		return _apiPath;
+	}
+
+	public static String getGCM() {
+		return GCM;
+	}
+
+	public static void setGCM(String GCM) {
+		Globals.GCM = GCM;
+	}
 }
