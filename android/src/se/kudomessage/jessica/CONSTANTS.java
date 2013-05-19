2415b67d10c6e1a9f39e3ea678f6861e778310d7
Alexander Hultnér <ahultner@gmail.com>
2013-05-20 01:40:47 +0200

    This is only the default address

--- android/src/se/kudomessage/jessica/CONSTANTS.java
+++ android/src/se/kudomessage/jessica/CONSTANTS.java
@@ -3,6 +3,6 @@ package se.kudomessage.jessica;
 public final class CONSTANTS {
 	public static final String TAG = "KMCS";
 	public static final String SENDER_ID = "509205524891";
-	public static final String SERVER_ADDRESS = "192.168.1.14";
+	public static final String DEFAULT_SERVER_ADDRESS = "192.168.1.14";
 	public static final int SERVER_PORT = 5001;
 }
