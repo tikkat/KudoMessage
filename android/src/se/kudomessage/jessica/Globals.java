958657dedc6c3c3cb969037b852fb42528e55b3d
Alexander Hultnér <ahultner@gmail.com>
2013-05-20 00:41:00 +0200

    Updated globals for SharedPreferences handling

--- android/src/se/kudomessage/jessica/Globals.java
+++ android/src/se/kudomessage/jessica/Globals.java
@@ -1,10 +1,74 @@
 package se.kudomessage.jessica;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 
-public class Globals {
-	public static Activity activity;
-	public static String GCMKey;
-	public static String accessToken;
-	public static String email;
+/**
+ * @author hultner
+ *
+ * Fetches and saves preferences using SharedPreferences when it makes sense to save them between sessions.
+ * 
+ */
+
+public class Globals{
+	
+	private static String accessToken;
+	private static String email;
+	private static Activity activity;
+	private static String packageIdentifier;
+	private static String apiPath = "http://172.20.10.5:8080/KudoMessage_Web_Application/api/rest/";
+	private static String GCM;
+	private static SharedPreferences prefs;
+
+	public static String getAccessToken() {
+		return prefs.getString(prefKey("accessToken"), null);
+	}
+
+	public static void setAccessToken(String accessToken) {
+		//Globals.accessToken = accessToken;
+		prefs.edit().putString(prefKey("accessToken"), accessToken).commit();
+	}
+
+	public static String getEmail() {
+		return prefs.getString(prefKey("email"), null);
+	}
+
+	public static void setEmail(String email) {
+		//Globals.email = email;
+		prefs.edit().putString(prefKey("email"), email).commit();
+	}
+	
+	public static Activity getActivity() {
+		return activity;
+	}
+
+	public static void setActivity(Activity activity) {
+		Globals.activity = activity;
+		Globals.packageIdentifier = activity.getPackageName();
+		Globals.prefs = activity.getSharedPreferences(
+				Globals.packageIdentifier, Context.MODE_PRIVATE);
+	}
+	
+	public static void setApiPath(String apiPath){
+		//Globals.apiPath = apiPath;
+		prefs.edit().putString(prefKey("apiPath"), apiPath).commit();
+	}
+
+	public static String getApiPath(){
+		return prefs.getString(prefKey("apiPath"), null);
+	}
+
+	public static String getGCM() {
+		return prefs.getString(prefKey("GCM"), null);
+	}
+
+	public static void setGCM(String GCM) {
+		//Globals.GCM = GCM;
+		prefs.edit().putString(prefKey("GCM"), GCM).commit();
+	}
+	
+	private static String prefKey(String pref){
+		return Globals.packageIdentifier+"."+pref;
+	}
 }
