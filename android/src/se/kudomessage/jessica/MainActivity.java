a661a5fdbeea9452fba199d40d46e4ac330aa620
Alexander Hultnér <ahultner@gmail.com>
2013-05-20 02:11:52 +0200

    Added buttonlisteners for the ui

--- android/src/se/kudomessage/jessica/MainActivity.java
+++ android/src/se/kudomessage/jessica/MainActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
@@ -42,5 +43,41 @@ public class MainActivity extends Activity {
 	
 		PushController.registerDevice();
 		getContentResolver().registerContentObserver(Uri.parse("content://sms/"), true, new SMSSentObserver(new Handler(), this));
+	}
+	
+	/** Button listener **/
+
+	/**
+	 * Triggered when the user clicks the "Sign in with Google" button
+	 * 
+	 * @param view required for button listeners 
+	 */
+	public void requestOAuth(View view) {
+		OAuthController.requestAccessToken();
+	}
+	
+	
+	/**
+	 * Triggered when the users clicks the "Continue" button
+	 * 
+	 * @param view required for button listeners 
+	 */
+	public void registerUser(View view){
+		// To be added some error control, check so that the server 
+		// address is a valid server and that the user have connected with google.
+		if(Globals.getAccessToken() != null ){
+			//Error stuff for accesstoken
+			Log.e("registerUser", "Accesstoken not granted");
+		}else if(PushController.testServer() != false){
+			//If the server isn't a valid KudoMessage server.
+			Log.e("registerUser", "The message server wasn't valid");
+		}else if(Globals.getGCM() != null){
+			//Please wait for GCM something message
+			Log.e("registerUser", "GCM wasn't fetched yet");
+		}else{
+			//Success
+			Log.d("registerUser", "Successfully");
+			init();
+		}
 	}
 }
