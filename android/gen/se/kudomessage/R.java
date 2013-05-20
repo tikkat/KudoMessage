c7a3b86f0af568f635680f23019e893ddde081ce
Alexander Hultnér <ahultner@gmail.com>
2013-05-20 16:42:12 +0200

    Added some stuff to be used in the gui

--- android/gen/se/kudomessage/R.java
+++ android/gen/se/kudomessage/R.java
@@ -35,7 +35,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int google_connect=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int reg=0x7f050006;
+        public static final int server=0x7f050005;
+        public static final int server_disclaimer=0x7f050004;
     }
     public static final class style {
         /** 
