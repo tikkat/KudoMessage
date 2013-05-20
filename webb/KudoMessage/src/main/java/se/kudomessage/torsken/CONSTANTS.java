09401ab6a2a084a5ef6e6d9645ff6b02c0691a1d
Philip Ekman <ekman1991@gmail.com>
2013-05-20 02:00:38 +0200

    Updated names

--- webb/KudoMessage/src/main/java/se/kudomessage/torsken/CONSTANTS.java
+++ webb/KudoMessage/src/main/java/se/kudomessage/torsken/CONSTANTS.java
@@ -6,5 +6,5 @@ public class CONSTANTS {
     
     public static final String CLIENT_ID = "509205524891-joksn0omcchbae38krmc1nnjmaqavc2n.apps.googleusercontent.com";
     public static final String CLIENT_SECRET = "pao6aVAL0JTkeAohEWDR2dXP";
-    public static final String REDIRECT_URI = "http://localhost:8080/KudoMessageCharlieStyle/oauthcallback/index.xhtml";
+    public static final String REDIRECT_URI = "http://localhost:8080/KudoMessage/oauthcallback/index.xhtml";
 }
