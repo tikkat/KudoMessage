06ea21bbc01e942475df13a6ce2315b16b065f00
Philip Ekman <ekman1991@gmail.com>
2013-05-21 15:15:43 +0200

    Moved socket init to sockethandler, new thread is better

--- webb/KudoMessage/src/main/java/se/kudomessage/torsken/BackendBean.java
+++ webb/KudoMessage/src/main/java/se/kudomessage/torsken/BackendBean.java
@@ -50,70 +50,7 @@ public class BackendBean {
 
             if (in != null && out != null) {
                 System.out.println("Connected to server " + CONSTANTS.SERVER_ADDRESS + ":" + CONSTANTS.SERVER_PORT);
-
-                // TODO: Do all things below in SocketHandler's constructor?
                 
-                out.println("CLIENT");
-                out.flush();
-
-                try {
-                    JSONObject output = new JSONObject();
-                    output.put("action", "init");
-                    output.put("token", Globals.accessToken);
-
-                    out.println(output.toString());
-                    out.flush();
-
-                    // First answer from server is the email.
-                    Globals.email = in.readLine();
-
-                    // Global PushRenderer
-                    Globals.pr = PushRenderer.getPortableRenderer();
-                    ConversationsHolder.getInstance().addViewToPush();
-
-                    // Get contacts
-                    output = new JSONObject();
-                    output.put("action", "get-contacts");
-
-                    out.println(output.toString());
-                    out.flush();
-
-                    JSONObject c = new JSONObject(in.readLine());
-                    JSONArray d = c.getJSONArray("contacts");
-
-                    for (int i = 0; i < d.length(); i++) {
-                        String name = d.getJSONObject(i).getString("name");
-                        String number = d.getJSONObject(i).getString("number");
-
-                        Globals.contacts.put(number, name);
-                    }
-
-                    // Load the first emails
-                    output = new JSONObject();
-                    output.put("action", "get-messages");
-                    output.put("lower", "0");
-                    output.put("upper", "5");
-
-                    out.println(output.toString());
-                    out.flush();
-                    
-                    JSONObject e = new JSONObject(in.readLine());
-                    JSONArray f = e.getJSONArray("messages");
-                    
-                    System.out.println("###### Got " + f.length());
-
-                    for (int i = 0; i < f.length(); i++) {
-                        KudoMessage message = new KudoMessage();
-                        message.content = f.getJSONObject(i).getString("content");
-                        message.origin = f.getJSONObject(i).getString("origin");
-                        message.addReceiver(f.getJSONObject(i).getString("receiver"));
-                        
-                        ConversationsHolder.getInstance().addMessage(message);
-                    }
-                } catch (Exception ex) {
-                    System.err.println("Something wrong in action init: " + ex.toString());
-                }
-
                 // Read new messages in new thread.
                 (new SocketHandler(socket, in, out)).start();
             }
--- webb/KudoMessage/src/main/java/se/kudomessage/torsken/SocketHandler.java
+++ webb/KudoMessage/src/main/java/se/kudomessage/torsken/SocketHandler.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import javax.faces.bean.ManagedBean;
+import org.icefaces.application.PushRenderer;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 @ManagedBean
@@ -21,6 +23,67 @@ public class SocketHandler extends Thread {
         SocketHandler.socket = socket;
         SocketHandler.in = in;
         SocketHandler.out = out;
+        
+        System.out.println("####### LOADED SOCKETHANDLER() ###");
+
+        out.println("CLIENT");
+        out.flush();
+
+        try {
+            JSONObject output = new JSONObject();
+            output.put("action", "init");
+            output.put("token", Globals.accessToken);
+
+            out.println(output.toString());
+            out.flush();
+
+            // First answer from server is the email.
+            Globals.email = in.readLine();
+
+            // Global PushRenderer
+            Globals.pr = PushRenderer.getPortableRenderer();
+            ConversationsHolder.getInstance().addViewToPush();
+
+            // Get contacts
+            output = new JSONObject();
+            output.put("action", "get-contacts");
+
+            out.println(output.toString());
+            out.flush();
+
+            JSONObject c = new JSONObject(in.readLine());
+            JSONArray d = c.getJSONArray("contacts");
+
+            for (int i = 0; i < d.length(); i++) {
+                String name = d.getJSONObject(i).getString("name");
+                String number = d.getJSONObject(i).getString("number");
+
+                Globals.contacts.put(number, name);
+            }
+
+            // Load the first emails
+            output = new JSONObject();
+            output.put("action", "get-messages");
+            output.put("lower", "0");
+            output.put("count", "5");
+
+            out.println(output.toString());
+            out.flush();
+
+            JSONObject e = new JSONObject(in.readLine());
+            JSONArray f = e.getJSONArray("messages");
+
+            for (int i = 0; i < f.length(); i++) {
+                KudoMessage message = new KudoMessage();
+                message.content = f.getJSONObject(i).getString("content");
+                message.origin = f.getJSONObject(i).getString("origin");
+                message.addReceiver(f.getJSONObject(i).getString("receiver"));
+
+                ConversationsHolder.getInstance().addMessage(message);
+            }
+        } catch (Exception ex) {
+            System.err.println("Something wrong in action init: " + ex.toString());
+        }
     }
 
     public static PrintWriter getOut() {
@@ -41,6 +104,18 @@ public class SocketHandler extends Thread {
 
             if (inputString == null || inputString.isEmpty()) {
                 continue;
+            }
+            
+            if (inputString.equals("CLOSE")) {
+                System.out.println("### GOT CLOSE ###");
+                
+                try {
+                    in.close();
+                    out.close();
+                    socket.close();
+                } catch (Exception e) {}
+
+                break;
             }
 
             try {
