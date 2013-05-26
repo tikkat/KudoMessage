5aac3d1492f8b90783116f4c5bc2aaeea402c738
Tobias Tikka <tobias.tikka@gmail.com>
2013-05-26 18:46:49 +0200

    Sound at login, jQuery and main.js
    Moved all javascript functions into a seperate file, main.js.
    Downloaded jQuery for local uses.
    Implemented a sound playing at login.

---
+++ webb/KudoMessage/src/main/webapp/js/main.js
@@ -0,0 +1,36 @@
+function showNewContactBox(button) {
+    if ($('#newContactBox').is(':visible')) {
+        hideNewContactBox();
+    } else {
+        var position = $(button).position();
+        var left = position.left + 20;
+        var top = position.top + 34;
+
+        $("#newContactBox").css({top: top + 'px', left: left + 'px'});
+        $("#newContactBox").slideDown("slow");
+    }
+}
+
+function hideNewContactBox() {
+    $("#newContactBox").hide();
+}
+
+function conversationOnHover(button) {
+    $(button).parent().css('background', '#F3F3F3');
+}
+
+function conversationOnNotHover(button) {
+    $(button).parent().css('background', 'transparent');
+}
+
+function showCross(div) {
+    $(div).children().children().css('visibility', 'visible');
+}
+
+function hideCross(div) {
+    $(div).children().children().css('visibility', 'hidden');
+}
+
+function setNewConversationTextBox(sender) {
+    $(".newConversationTextBox").val($(sender).val());
+}
